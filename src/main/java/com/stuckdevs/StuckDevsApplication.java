de712fb46e0a776df61c22c40ae5e530a64e8fa5
sourabhgorule <gorulesourabh@gmail.com>
2024-07-23 09:02:15 +0530

    initial commit

---
+++ src/main/java/com/stuckdevs/StuckDevsApplication.java
@@ -0,0 +1,13 @@
+package com.stuckdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuckDevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuckDevsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stuckdevs/StuckDevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuckdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuckDevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
